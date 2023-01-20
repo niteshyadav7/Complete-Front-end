1c075b7a0b1d4ab43e6d96e4179f1a0ad2a8bc67
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-01-21 01:13:49 +0530

    learnings

--- JS/Function/script.js
+++ JS/Function/script.js
@@ -9,20 +9,50 @@
 // transform('HELLO ! HOW ARE YOU', toMake);
 /*-------------------👋👋👋👋👋👋👋👋    Call back functions(H.O.F)    👋👋👋👋👋👋👋👋👋👋👋----------------------------------*/
 // make a function to lower the letters
-const toMakeLower = function (str) {
-  const transformString = str.toLowerCase();
-  return transformString;
-};
+// const toMakeLower = function (str) {
+//   const transformString = str.toLowerCase();
+//   return transformString;
+// };
 
-// Use callBacks
-const convert = function (str, fun) {
-  // BEFORE
-  console.log(`${str}`);
-  // AFTER
-  console.log(`${fun(str)}`);
-};
-convert(
-  'HEY, WHAT ARE YOU DOING RIGHT NOW? \rg ARE YOU FEELING GOOD',
-  toMakeLower
-);
+// // Use callBacks
+// const convert = function (str, fun) {
+//   // BEFORE
+//   console.log(`${str}`);
+//   // AFTER
+//   console.log(`${fun(str)}`);
+// };
+// convert(
+//   'HEY, WHAT ARE YOU DOING RIGHT NOW? \rg ARE YOU FEELING GOOD',
+//   toMakeLower
+// );
 /*-------------------👋👋👋👋👋👋👋👋    function returning functions(H.O.F)    👋👋👋👋👋👋👋👋👋👋👋---------------------------------*/
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+// const ans = greet('Hey!');
+// // console.log(ans);
+// ans('Jonas');
+// ans('NItesh');
+
+// we can also write in arrow Function:
+// const greet = greeting => namee => console.log(`${greeting} ${namee}`);
+// greet('Hey!')('NITESH');
+
+/*-------------------👋👋👋👋👋👋👋👋    The Call and Apply Method    👋👋👋👋👋👋👋👋👋👋👋---------------------------------*/
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNum} `
+    );
+    this.bookings.push({ flight: `${this.airline} ${flightNum} `, name });
+  },
+};
+lufthansa.book(239, 'Jonas');
+lufthansa.book(234, 'Nitesh');
+console.log(lufthansa);
