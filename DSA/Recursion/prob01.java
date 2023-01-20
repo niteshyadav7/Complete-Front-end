3a0c691066120cb938c0f29fb2a60417a787c08a
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-01-20 06:55:32 +0530

    problem2

--- DSA/Recursion/prob01.java
+++ DSA/Recursion/prob01.java
@@ -2,9 +2,15 @@ package DSA.Recursion;
 // import java.io.*;
 import java.util.Scanner;
 public class prob01 {
+    public static void printIncreasing (int num){
+        if(num == 0)
+        return;
+        printIncreasing(num-1);
+        System.out.println(num);
+    }
     public static void main(String[] args) {
         Scanner scn= new Scanner (System.in);
-        int num =scn.nextInt();
-        System.out.println(num+22);
+        int n = scn.nextInt();
+        printIncreasing(n);
     }
 }
