051b0fb9c44993040bcc8c3804022b067ed76382
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-01-20 06:37:14 +0530

    problem01

---
+++ DSA/Recursion/prob01.java
@@ -0,0 +1,10 @@
+package DSA.Recursion;
+// import java.io.*;
+import java.util.Scanner;
+public class prob01 {
+    public static void main(String[] args) {
+        Scanner scn= new Scanner (System.in);
+        int num =scn.nextInt();
+        System.out.println(num+22);
+    }
+}
