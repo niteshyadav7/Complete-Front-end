7a07d01e2eba4bf77074942197b1f8a66b9c4a22
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-01-21 20:09:53 +0530

    change

---
+++ JS/Revision/script.js
@@ -0,0 +1,53 @@
+// Calculate maximum
+// const arr = [-1, 4, -8, -34, 77];
+// let max = -Infinity;
+// for (let i = 0; i < arr.length; i++) {
+//   if (max < arr[i]) {
+//     max = arr[i];
+//   }
+//   //   console.log(max);
+// }
+// console.log(max);
+// const spanOfArray = function (arr) {
+//   let max = -Infinity;
+//   let min = Infinity;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (max < arr[i]) {
+//       max = arr[i];
+//     }
+//     if (min > arr[i]) {
+//       min = arr[i];
+//     }
+//   }
+//   return max + min;
+// };
+// let arr = [-1, 4, -8, -34, 77];
+// let ans = spanOfArray(arr);
+// console.log(ans);
+////////////////////////////////////////////////////
+// HOW TO DEBUGE THE PROBLEMS
+// const measureKelvin = function () {
+//   const temp = {
+//     type: "temperature",
+//     unit: "celsius",
+//     value: prompt("Value in  celsius:"),
+//   };
+//   const convertInKelvin = Number(temp.value) + 273;
+//   return convertInKelvin;
+// };
+// console.log(measureKelvin());°
+// coding Callenge:1
+// const data1 = [17, 21, 23];
+// const data2 = [12, 5, -5, 0, 4];
+// const data = [...data1, ...data2];
+// // console.log(data);
+// function printForecast(data) {
+//   let str = "";
+//   for (let i = 0; i < data.length; i++) {
+//     str += `...${data[i]}°C in ${i + 1} days`;
+//     // console.log(str);
+//   }
+//   console.log(str);
+// }
+// printForecast(data);
+// ---------------------🤗🤗🤗🤗🤗🤗🤗🤗🤗🤗🤗🤗🤗🤗🤗🤗🤗--------------------------------------
